64ca3f39afca73cad7853939ce90c02cedb06a8b
hmtmcse <hmtm.cse@gmail.com>
2023-10-08 23:24:40 +0600

    added new methods

--- ppy_file_text/text/ppyt_string_util.py
+++ ppy_file_text/text/ppyt_string_util.py
@@ -19,7 +19,10 @@ class StringUtil:
 
     @staticmethod
     def camelcase_to(text: str, to: str = "_"):
-        return re.sub(r'(?<!^)(?=[A-Z])', to, text)
+        text = str(text)
+        text = text.strip()
+        text = re.sub(r'(?<!^)(?=[A-Z])', to, text)
+        return text
 
     @staticmethod
     def remove_special_character(text: str, to: str = ""):
@@ -55,13 +58,49 @@ class StringUtil:
         text = StringUtil.find_and_replace_with(text, "-", " ")
         text = text.strip()
         text = text.title()
-        return text
+        return StringUtil.replace_space_with(text, " ")
+
+    @staticmethod
+    def replace_multiple_occurrence_to_single_with(text: str, to: str = "_"):
+        return re.sub(f"{to}+", to, text)
 
     @staticmethod
     def system_readable(text: str):
         text = StringUtil.camelcase_to(copy(text), "_")
         text = StringUtil.find_and_replace_with(text, " ", "_")
+        text = StringUtil.replace_multiple_occurrence_to_single_with(text=text, to="_")
         text = text.strip()
         text = text.lower()
         text = re.sub(r'[^a-zA-Z0-9_]', '', text)
         return text
+
+    @staticmethod
+    def remove_leading_number(text: str):
+        return re.sub("^\d+", '', text)
+
+    @staticmethod
+    def lower_first_char(text: str):
+        if not text:
+            return ""
+        text = str(text)
+        return text[0].lower() + text[1:]
+
+    @staticmethod
+    def py_underscore_name(name: str):
+        name = StringUtil.lower_first_char(text=name)
+        name = StringUtil.system_readable(name)
+        name = StringUtil.remove_special_character(name)
+        name = StringUtil.remove_leading_number(name)
+        return name
+
+    @staticmethod
+    def py_hyphen_name(name: str):
+        name = StringUtil.py_underscore_name(name=name)
+        name = StringUtil.find_and_replace_with(text=name, find="_", replace="-")
+        return name
+
+    @staticmethod
+    def py_class_name(name: str):
+        name = StringUtil.py_underscore_name(name=name)
+        name = StringUtil.underscore_to_camelcase(word=name)
+        return name
