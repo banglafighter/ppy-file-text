0fded98197f8a139beacffcb703aadb254ad8470
hmtmcse <hmtm.cse@gmail.com>
2023-10-11 07:01:48 +0600

    Updated version for initial release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_dependencies():
 
 setup(
     name='ppy-file-text',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/problemfighter/ppy-file-text',
     license='Apache 2.0',
     author='Problem Fighter',
